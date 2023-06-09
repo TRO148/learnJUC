9c1d6290057c274a9479613334e5a732fc6ea201
trotro <1481895142@qq.com>
2023-06-09 20:22:55 +0800

    第一章

---
+++ src/main/java/ln/Study.java
@@ -0,0 +1,19 @@
+package ln;
+
+import ln.n1.n1;
+import ln.n2.n2;
+
+/**
+ * 学习介绍
+ * 嘿嘿，用来学习JUC，比较好用的笔记方式
+ * @author TroTro
+ * <p/>
+ * {@link ln.n1.n1} 第一节，简单介绍线程进程和同步异步
+ * {@link ln.n2.n2} 第二节，线程的一系列方法，和基本使用
+ */
+public class Study {
+    public static void main(String[] args) {
+        n1 n1 = new n1();
+        n2 n2 = new n2();
+    }
+}
--- src/main/java/ln/n1/n1.java
+++ src/main/java/ln/n1/n1.java
@@ -6,26 +6,22 @@ import lombok.extern.slf4j.Slf4j;
  * 本节简单介绍和使用
  * {@link Sync}是同步的调度
  * {@link Async}是异步的调度
+ * @author TroTro
+ *
+ * 线程和进程的关系：
+ * 进程是指一个程序的运行，比如运行软件，运行个项目，运行代码。
+ * 线程是指一个进程中的一个执行流程，比如运行一个项目的时候，代码一趟走下来，是在线程上，也可以让代码分开执行，就是在多线程上。
+ * <p>
+ * 串行和并发的关系：
+ * 串行是指一个线程一个线程的执行，比如一个人做事，先做完一件事，再做另一件事。
+ * 并发是指多个线程同时执行，比如一个人做事，同时做多件事。
  */
 @Slf4j(topic = "n1")
-class n1 {
+public class n1 {
     public static void main(String[] args) {
         Sync.test();
         System.out.println("=========");
         Async.test();
-    }
-}
-
-@Slf4j(topic = "Sleep")
-class Sleep {
-    static public void test() {
-        log.info("开始睡大觉");
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        log.info("结束睡觉");
     }
 }
 
@@ -50,5 +46,18 @@ class Async {
     public static void test() {
         new Thread(Sleep::test).start();
         log.debug("异步");
+    }
+}
+
+@Slf4j(topic = "Sleep")
+class Sleep {
+    static public void test() {
+        log.info("开始睡大觉");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        log.info("结束睡觉");
     }
 }
